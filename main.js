544b92a5b9651ec74360fd24a8e30dce8ef70bc1
jyfoo213 <jyfoo213@gmail.com>
2020-09-13 23:49:34 +0800

    truncateUrl

--- main.js
+++ main.js
@@ -5,35 +5,36 @@ const searchBtn = document.getElementById('search-btn');
 const searchInput = document.getElementById('search-input');
 
 searchForm.addEventListener('submit', e => {
-  // Get sort
+  // get sort by:
   const sortBy = document.querySelector('input[name="sortby"]:checked').value;
-  // Get limit
+  // get results per page
   const searchLimit = document.getElementById('limit').value;
-  // Get search
+  // get search input
   const searchTerm = searchInput.value;
-  // Check for input
+  // check for search input
   if (searchTerm == '') {
-    // Show message
+  // Show message
     showMessage('Please enter search input', 'alert-danger');
   }
-  // Clear field
+  // clear search field after user press submit
   searchInput.value = '';
 
-  // Search Reddit
+  // search reddit; pass following parameters to reddit API
   reddit.search(searchTerm, searchLimit, sortBy).then(results => {
     let output = '<div class="card-columns">';
     console.log(results);
     results.forEach(post => {
-      // Check for image
+      // check if reddit post has image. if no image, display default image
       let image = post.preview
         ? post.preview.images[0].source.url
         : 'https://redditupvoted.files.wordpress.com/2019/10/dxfmwbkq.png';
       output += `
       <div class="card">
-      <h6 class="card-header bg-white">${truncateString(post.title, 200)}</h6>
+      <h5 class="card-header bg-white">${truncateString(post.title, 200)}</h5>
       <img class="card-img-top" src="${image}" alt="Reddit Card Image">
       <div class="card-body">
-        <p class="card-text small">${truncateString(post.selftext, 200)}
+        <p class="card-text">${truncateString(post.selftext, 200)}
+        <a href="${(post.url)}" target="_blank">${truncateUrl(post.url)}</a>
         <a href="https://reddit.com${post.permalink}" target="_blank"><b>View in Reddit</b></a>
       </div>
       <div class="card-footer bg-white">
@@ -75,4 +76,13 @@ function truncateString(myString, limit) {
   const shortened = myString.indexOf(' ', limit);
   if (shortened == -1) return myString;
   return myString.substring(0, shortened);
+}
+
+// Truncate URL Function
+var truncateUrl = function(u) {
+  uend = u.slice(u.length - 15);
+  ustart = u.replace('http://', '').replace('https://', '').substr(0, 30);
+  var shorter = ustart + '...' + uend;
+  alert(shorter);
+  return shorter;
 }
