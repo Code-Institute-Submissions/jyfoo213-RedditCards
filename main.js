467a160a371b76191a574dca0dc78a9487e4e169
jyfoo213 <jyfoo213@gmail.com>
2020-09-14 00:13:49 +0800

    shorturl

--- main.js
+++ main.js
@@ -79,6 +79,7 @@ function truncateString(myString, limit) {
   return myString.substring(0, shortened);
 }
 
+//Truncate URL Function
 function shortUrl(url, l){
   var l = typeof(l) != "undefined" ? l : 50;
   var chunk_l = (l/2);
@@ -89,4 +90,20 @@ function shortUrl(url, l){
   var start_chunk = shortString(url, chunk_l, false);
   var end_chunk = shortString(url, chunk_l, true);
   return start_chunk + ".." + end_chunk;
+}
+function shortString(s, l, reverse){
+  var stop_chars = [' ','/', '&'];
+  var acceptable_shortness = l * 0.80; // When to start looking for stop characters
+  var reverse = typeof(reverse) != "undefined" ? reverse : false;
+  var s = reverse ? s.split("").reverse().join("") : s;
+  var short_s = "";
+
+  for(var i=0; i < l-1; i++){
+      short_s += s[i];
+      if(i >= acceptable_shortness && stop_chars.indexOf(s[i]) >= 0){
+          break;
+      }
+  }
+  if(reverse){ return short_s.split("").reverse().join(""); }
+  return short_s;
 }
